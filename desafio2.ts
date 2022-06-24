9251cdb4c3770e32bd64d324aef1b6b89fa101e9
Clayton Joaquim <87860753+cjsantana01@users.noreply.github.com>
2022-06-24 11:57:31 -0300

    Update desafio2.ts

--- desafio2.ts
+++ desafio2.ts
@@ -1,7 +1,35 @@
-const employee = (nome: string, code: number) => {
-    nome: 'Jonh'
-    code: 25
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: string,
+}
+
+enum Profissao {
+    Padeiro,
+    Atriz
     
-};
+}
 
+const maria: Pessoa = {
+    nome: 'Maria',
+    idade: 23,
+    profissao: 'atriz'
+}
 
+const roberto: Pessoa = {
+    nome: 'Roberto',
+    idade: 19,
+    profissao: 'Padeiro'
+}   
+
+const laura: Pessoa = {
+    nome: 'Laura',
+    idade: 32,
+    profissao: 'Atriz'
+}   
+
+const carlos: Pessoa = {
+    nome: 'Carlos',
+    idade: 19,
+    profissao: 'padeiro'
+}   
